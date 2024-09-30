8ab2e0b9e6be713f41bdf0b1894763b003198e74
V2 dev <v2.softwarehouse@gmail.com>
2024-09-30 19:25:38 -0300

    review request google

--- handler.ts
+++ handler.ts
@@ -6,17 +6,17 @@ export const hello: APIGatewayProxyHandler = async (event, _context) => {
     let targetUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.ENGINE_API_KEY}`
     
     // request call with parameters
-    request(targetUrl, function (err, body) {
+    const result = request(targetUrl, function (err, body) {
         console.log('error:', err); // prints the error if one occurred
         let geoLocation = JSON.parse(body);
         let mssg = `lat: ${geoLocation.results[0].geometry.location.lat} long: ${geoLocation.results[0].geometry.location.lng}`;
         console.log(mssg);
-    }); 
+    }).await()
 
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+            message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!' + result,
             input: event,
         }),
     };
@@ -28,4 +28,15 @@ export const nuvem: Handler = async (mensagem: String, context: Context) => {
 
 export const novoArquivoJSON: Handler = async (event: APIGatewayProxyEvent, _context: Context) => {
   console.info(event);
+}
+
+const runner = async() => {
+    try {
+    const response = await firstRequest();
+    // At this point you can assume that the first request is completed successfully and you can extract what you need from the response.
+    const number = response.number 
+    await secondRequest(number);
+    } catch(error) {
+        console.log(error):
+    }
 }
